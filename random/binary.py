7d3c32ccdc173e5da268d273977c19d54b80f4a9
Roberto Trujillo <rotrujillo@pdc-prd-kvm-001-dracdflt.openmarket.com>
2021-05-14 17:15:20 -0500

    Add binary search

---
+++ random/binary.py
@@ -0,0 +1,25 @@
+import math
+
+"""
+    Given an array, with a set of  numbers perform a search in 
+    O(logn)
+    
+"""
+
+def binary_search(arr, key):
+    if len(arr) == 1:
+        if arr[0] == key:
+            return True
+        else:
+            return False
+    mid     = math.ceil(len(arr)/2)
+    value   = arr[mid] 
+    if value>key:
+        return binary_search(arr[0:mid], key)
+    else:
+        return binary_search(arr[mid:len(arr)], key) 
+
+arr     = [0,1,2,4,8,9,10,100]
+key     = 4
+output  = binary_search(numbers,key)
+print(output);
